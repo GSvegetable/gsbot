57faefad0d8fc3b98a02acfac762d60607cc8893
GSvegetable <3301296046@qq.com>
2026-07-18 06:45:26 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# ===== 配置信息 =====
+BOT_TOKEN = "8922179149:AAFy_m3SI1StQjn66ZGyIHQ3sLK1iKXINRw"
+AI_API_KEY = "sk-258fd45a189545d6b0d2b383f14094a9"
+AI_BASE_URL = "https://api.deepseek.com/chat/completions"
+AI_MODEL = "deepseek-chat"
+REQUIRED_CHANNEL = "gs0z1"
+ADMIN_CHAT_ID = 7857605443
