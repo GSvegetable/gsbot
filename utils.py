8ea072e4767e864497c5108c6c8a3b7eca0e706f
GSvegetable <3301296046@qq.com>
2026-07-18 06:46:46 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,44 +5,56 @@ def get_text(user_id, key, user_ui_lang):
     lang_code = user_ui_lang.get(user_id, 'zh')
     return UI_LANGUAGES[lang_code].get(key, key)
 
-# 主页键盘
 def get_main_keyboard(user_id, user_ui_lang):
-    keyboard = [
-        [InlineKeyboardButton(get_text(user_id, 'custom_btn', user_ui_lang), callback_data='custom_btn')],
+    keyboard = [[InlineKeyboardButton(get_text(user_id, 'robot_dev', user_ui_lang), callback_data='custom_btn')],
         [InlineKeyboardButton(get_text(user_id, 'contact_btn', user_ui_lang), callback_data='contact')],
-        [InlineKeyboardButton(get_text(user_id, 'gsai', user_ui_lang), callback_data='gsai'), InlineKeyboardButton(get_text(user_id, 'setting', user_ui_lang), callback_data='setting')]
-    ]
+        [InlineKeyboardButton(get_text(user_id, 'gsai', user_ui_lang), callback_data='gsai'), InlineKeyboardButton(get_text(user_id, 'setting', user_ui_lang), callback_data='setting')]]
     return InlineKeyboardMarkup(keyboard)
 
-# 定制子菜单键盘
-def get_custom_keyboard(user_id, user_ui_lang):
+def get_dev_keyboard(user_id, user_ui_lang):
+    keyboard = [[InlineKeyboardButton(get_text(user_id, 'dev_types', user_ui_lang), callback_data='dev_types')],
+        [InlineKeyboardButton(get_text(user_id, 'dev_captcha', user_ui_lang), callback_data='dev_captcha')],
+        [InlineKeyboardButton(get_text(user_id, 'back_msg', user_ui_lang), callback_data='back_home')]]
+    return InlineKeyboardMarkup(keyboard)
+
+def get_type_keyboard(user_id, user_ui_lang):
+    funcs = [("双向联系", 'contact'), ("宫水AI", 'gsai'), ("设置", 'setting'), ("群管", 'group_manage'), ("查询", 'query'), ("资源", 'resource'), ("签到", 'checkin'), ("AI", 'ai_sub')]
+    keyboard = []
+    for i in range(0, len(funcs), 2):
+        row = [InlineKeyboardButton(funcs[i][0], callback_data=funcs[i][1])]
+        if i + 1 < len(funcs): row.append(InlineKeyboardButton(funcs[i+1][0], callback_data=funcs[i+1][1]))
+        keyboard.append(row)
+    keyboard.append([InlineKeyboardButton(get_text(user_id, 'back_dev_menu', user_ui_lang), callback_data='back_dev_menu')])
+    return InlineKeyboardMarkup(keyboard)
+
+def get_captcha_keyboard(user_id, user_ui_lang):
     keyboard = [
-        [InlineKeyboardButton(get_text(user_id, 'group_manage', user_ui_lang), callback_data='group_manage'), InlineKeyboardButton(get_text(user_id, 'contact_sub', user_ui_lang), callback_data='contact_sub')],
-        [InlineKeyboardButton(get_text(user_id, 'query', user_ui_lang), callback_data='query'), InlineKeyboardButton(get_text(user_id, 'resource', user_ui_lang), callback_data='resource')],
-        [InlineKeyboardButton(get_text(user_id, 'checkin', user_ui_lang), callback_data='checkin'), InlineKeyboardButton(get_text(user_id, 'ai_sub', user_ui_lang), callback_data='ai_sub')],
-        [InlineKeyboardButton(get_text(user_id, 'back_btn', user_ui_lang), callback_data='back_home')]
-    ]
+        [InlineKeyboardButton(get_text(user_id, 'captcha_math', user_ui_lang), callback_data='captcha_math')],
+        [InlineKeyboardButton(get_text(user_id, 'back_dev_menu', user_ui_lang), callback_data='back_dev_menu')]]
+    return InlineKeyboardMarkup(keyboard)
+
+def get_math_result_keyboard(user_id, user_ui_lang):
+    keyboard = [
+        [InlineKeyboardButton(get_text(user_id, 'back_dev_menu', user_ui_lang), callback_data='back_dev_menu')],
+        [InlineKeyboardButton(get_text(user_id, 'back_msg', user_ui_lang), callback_data='back_home')]]
     return InlineKeyboardMarkup(keyboard)
 
 def get_setting_keyboard(user_id, user_ui_lang):
     keyboard = [
         [InlineKeyboardButton(get_text(user_id, 'setting_lang', user_ui_lang), callback_data='setting_lang')],
-        [InlineKeyboardButton(get_text(user_id, 'setting_back', user_ui_lang), callback_data='back_home')]
-    ]
+        [InlineKeyboardButton(get_text(user_id, 'setting_back', user_ui_lang), callback_data='back_home')]]
     return InlineKeyboardMarkup(keyboard)
 
 def get_lang_keyboard(user_id, user_ui_lang):
     keyboard = [
         [InlineKeyboardButton(get_text(user_id, 'lang_zh', user_ui_lang), callback_data='lang_zh'), InlineKeyboardButton(get_text(user_id, 'lang_en', user_ui_lang), callback_data='lang_en')],
-        [InlineKeyboardButton(get_text(user_id, 'lang_back', user_ui_lang), callback_data='lang_back')]
-    ]
+        [InlineKeyboardButton(get_text(user_id, 'lang_back', user_ui_lang), callback_data='lang_back')]]
     return InlineKeyboardMarkup(keyboard)
 
 def get_channel_keyboard(user_id, user_ui_lang, channel_link):
     keyboard = [
         [InlineKeyboardButton(get_text(user_id, 'join_btn', user_ui_lang), url=channel_link)],
-        [InlineKeyboardButton(get_text(user_id, 'check_btn', user_ui_lang), callback_data='check_member')]
-    ]
+        [InlineKeyboardButton(get_text(user_id, 'check_btn', user_ui_lang), callback_data='check_member')]]
     return InlineKeyboardMarkup(keyboard)
 
 def get_chat_reply_keyboard():
